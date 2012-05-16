74253113d2e4914988a169596d97fd61ba712c5a
CJKay <cjkay93@gmail.com>
2012-05-16 20:12:31 +0100

    Completed IFB and IFC optimisations

--- lib/Target/DCPU16/DCPU16Peephole.cpp
+++ lib/Target/DCPU16/DCPU16Peephole.cpp
@@ -1,31 +1,31 @@
 //===-- DCPU16Peephole.cpp - DCPU16 Peephole Optimiztions ---------------===//
 //
-//										 The LLVM Compiler Infrastructure
+//                     The LLVM Compiler Infrastructure
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 //
 // This peephole pass optimizes in the following cases.
 // 1. Optimizes redundant sign extends for the following case
-//		Transform the following pattern
-//		%vreg170<def> = SXTW %vreg166
-//		...
-//		%vreg176<def> = COPY %vreg170:subreg_loreg
+//    Transform the following pattern
+//    %vreg170<def> = SXTW %vreg166
+//    ...
+//    %vreg176<def> = COPY %vreg170:subreg_loreg
 //
-//		Into
-//		%vreg176<def> = COPY vreg166
+//    Into
+//    %vreg176<def> = COPY vreg166
 //
-//	2. Optimizes redundant negation of predicates.
-//		 %vreg15<def> = CMPGTrr %vreg6, %vreg2
-//		 ...
-//		 %vreg16<def> = NOT_p %vreg15<kill>
-//		 ...
-//		 JMP_c %vreg16<kill>, <BB#1>, %PC<imp-def,dead>
+//  2. Optimizes redundant negation of predicates.
+//     %vreg15<def> = CMPGTrr %vreg6, %vreg2
+//     ...
+//     %vreg16<def> = NOT_p %vreg15<kill>
+//     ...
+//     JMP_c %vreg16<kill>, <BB#1>, %PC<imp-def,dead>
 //
-//		 Into
-//		 %vreg15<def> = CMPGTrr %vreg6, %vreg2;
-//		 ...
-//		 JMP_cNot %vreg15<kill>, <BB#1>, %PC<imp-def,dead>;
+//     Into
+//     %vreg15<def> = CMPGTrr %vreg6, %vreg2;
+//     ...
+//     JMP_cNot %vreg15<kill>, <BB#1>, %PC<imp-def,dead>;
 //
 // Note: The peephole pass makes the instrucstions like
 // %vreg170<def> = SXTW %vreg166 or %vreg16<def> = NOT_p %vreg15<kill>
@@ -61,202 +61,174 @@
 using namespace llvm;
 
 static cl::opt<bool> DisableDCPU16Peephole(
-	"disable-dcpu16-peephole",
-	cl::Hidden,
-	cl::ZeroOrMore,
-	cl::init(false),
-	cl::desc("Disable Peephole Optimisations")
+  "disable-dcpu16-peephole",
+  cl::Hidden,
+  cl::ZeroOrMore,
+  cl::init(false),
+  cl::desc("Disable Peephole Optimisations")
 );
 
 static cl::opt<bool> DisableOptBrcc(
-	"disable-dcpu16-brcc",
-	cl::Hidden, cl::ZeroOrMore, cl::init(false),
-	cl::desc("Disable Conditional Branch Optimization")
+  "disable-dcpu16-brcc",
+  cl::Hidden, cl::ZeroOrMore, cl::init(false),
+  cl::desc("Disable Conditional Branch Optimization")
 );
 
 namespace {
-	struct DCPU16Peephole : public MachineFunctionPass {
-		const DCPU16InstrInfo		*QII;
-		const DCPU16RegisterInfo	*QRI;
-		const MachineRegisterInfo	*MRI;
+  struct DCPU16Peephole : public MachineFunctionPass {
+    const DCPU16InstrInfo    *QII;
+    const DCPU16RegisterInfo  *QRI;
+    const MachineRegisterInfo  *MRI;
 
-	public:
-		static char ID;
-		DCPU16Peephole() : MachineFunctionPass(ID) { }
+  public:
+    static char ID;
+    DCPU16Peephole() : MachineFunctionPass(ID) { }
 
-		bool runOnMachineFunction(MachineFunction &MF);
-		void runOptBrcc(MachineBasicBlock *mbb);
+    bool runOnMachineFunction(MachineFunction &MF);
+    void runOptBrcc(MachineBasicBlock *mbb);
+    bool swapOptBrcc(MachineInstr *brInstr, MachineInstr *andInstr);
 
-		const char *getPassName() const {
-			return "DCPU16 optimize conditional branches";
-		}
+    const char *getPassName() const {
+      return "DCPU16 optimize conditional branches";
+    }
 
-		void getAnalysisUsage(AnalysisUsage &AU) const {
-			MachineFunctionPass::getAnalysisUsage(AU);
-		}
-	};
+    void getAnalysisUsage(AnalysisUsage &AU) const {
+      MachineFunctionPass::getAnalysisUsage(AU);
+    }
+  };
 }
 
 char DCPU16Peephole::ID = 0;
 
+bool DCPU16Peephole::swapOptBrcc(MachineInstr *brInstr, MachineInstr *andInstr) {
+  MachineOperand &brA = brInstr->getOperand(1);
+  MachineOperand &brB = brInstr->getOperand(2);
+  MachineOperand &andA = andInstr->getOperand(1);
+  MachineOperand &andB = andInstr->getOperand(2);
+  
+  if(brA.isReg() && andA.isReg() && brB.isReg() && andB.isReg()) {
+    brA.setReg(andA.getReg());
+    brB.setReg(andB.getReg());
+    return true;
+  } else if(brA.isReg() && andA.isReg() && brB.isImm() && andB.isImm()) {
+    brA.setReg(andA.getReg());
+    brB.setImm(andB.getImm());
+    return true;
+  } else {
+    assert("Encountered unexpected combination in swapOptBrcc");
+    return false;
+  }
+}
+
 void DCPU16Peephole::runOptBrcc(MachineBasicBlock *mbb) {
-	// Dump the block (remove when done)
-	mbb->dump();
-	std::cout << std::endl;
-	
-	DenseMap<unsigned, MachineInstr *> peepholeMap;
-	
-	for(MachineBasicBlock::iterator miiIter = mbb->begin(); miiIter != mbb->end(); ++miiIter) {
-		MachineInstr *instruction = miiIter;	
-		
-		switch(instruction->getOpcode()) {
-			// And instructions
-			// AND register, immediate
-			case DCPU16::AND16ri: {
-				assert(instruction->getNumOperands() == 4);
-				
-				// %vreg1<def> = AND16ri %vreg0, [immediate], %EX<imp-def,dead>; GR16:%vreg1,%vreg0
-				MachineOperand &result = instruction->getOperand(0);
-				unsigned resultReg = result.getReg();
-				
-				if(TargetRegisterInfo::isVirtualRegister(resultReg))
-					peepholeMap[resultReg] = instruction;
-				
-				break;
-			}
-			
-			// Branch instructions
-			case DCPU16::BR_CCri: {
-				switch(instruction->getOperand(0).getImm()) {
-					case DCPU16CC::COND_NE: {
-						assert(instruction->getNumOperands() == 4);
-						
-						// BR_CCri 3, %vreg1, 0, <BB#2>; GR16:%vreg1
-						MachineOperand &brImm = instruction->getOperand(0);
-						MachineOperand &oldA = instruction->getOperand(1);
-						MachineOperand &oldB = instruction->getOperand(2);
-						if(oldB.getImm() != 0) break; // lhs == 0
-						
-						MachineOperand &regOperand = instruction->getOperand(1);
-						unsigned vReg = regOperand.getReg();
-						
-						if(TargetRegisterInfo::isVirtualRegister(vReg)) {
-							if(MachineInstr *peepholeSource = peepholeMap.lookup(vReg)) {
-								MachineOperand &newA = peepholeSource->getOperand(1);
-								MachineOperand &newB = peepholeSource->getOperand(2);
-								assert(newA.isReg() && newB.isImm() && "Encountered wrong operands in DCPU-16 BR_CCri peephole opt");
-								
-								unsigned vReg = regOperand.getReg();
-								
-								// Change the IFN to IFB and swap the operands
-								brImm.setImm(DCPU16CC::COND_B);
-								oldA.setReg(newA.getReg());
-								oldB.setImm(newB.getImm());
-								
-								// Remove the AND
-								peepholeSource->eraseFromParent();
-								
-								std::cout << "Changed to:" << std::endl;
-								instruction->dump();
-								std::cout << std::endl;
-							}
-						}
-						
-						break;
-					}
-				}
-			}
-		}
-	}
+  // Dump the block (remove when done)
+  mbb->dump();
+  std::cout << std::endl;
+  
+  DenseMap<unsigned, MachineInstr *> peepholeMap;
+  
+  for(MachineBasicBlock::iterator miiIter = mbb->begin(); miiIter != mbb->end(); ++miiIter) {
+    MachineInstr *instruction = miiIter;  
+    
+    switch(instruction->getOpcode()) {
+      // And instructions
+      case DCPU16::AND16rr:
+      case DCPU16::AND16ri: {
+        assert(instruction->getNumOperands() == 4);
+        
+        MachineOperand &result = instruction->getOperand(0);
+        unsigned resultReg = result.getReg();
+        
+        if(TargetRegisterInfo::isVirtualRegister(resultReg))
+          peepholeMap[resultReg] = instruction;
+        
+        break;
+      }
+      
+      // Branch instructions
+      case DCPU16::BR_CCri: {
+        switch(instruction->getOperand(0).getImm()) {
+          case DCPU16CC::COND_NE: {
+            assert(instruction->getNumOperands() == 4);
+            
+            if(instruction->getOperand(2).getImm() != 0) break; // Only works if comparing to 0
+            
+            unsigned activeReg = instruction->getOperand(1).getReg();
+            
+            if(MachineInstr *peepholeSource = peepholeMap.lookup(activeReg)) {
+              std::cout << "--------------------------------------------------------" << std::endl;
+              std::cout << "Optimisation opportunity:" << std::endl;
+              peepholeSource->dump();
+              instruction->dump();
+              std::cout << std::endl;
+              
+              // Change the IFN to IFB and swap the operands
+              instruction->getOperand(0).setImm(DCPU16CC::COND_B);
+              swapOptBrcc(instruction, peepholeSource);
+              
+              // Remove the AND from the block
+              peepholeSource->eraseFromParent();
+              
+              std::cout << "Changed to:" << std::endl;
+              instruction->dump();
+              std::cout << "--------------------------------------------------------" << std::endl << std::endl;;
+            }
+            
+            break;
+          }
+          
+          case DCPU16CC::COND_E: {
+            assert(instruction->getNumOperands() == 4);
+            
+            if(instruction->getOperand(2).getImm() != 0) break; // Only works if comparing to 0
+            
+            unsigned activeReg = instruction->getOperand(1).getReg();
+            
+            if(MachineInstr *peepholeSource = peepholeMap.lookup(activeReg)) {
+              std::cout << "--------------------------------------------------------" << std::endl;
+              std::cout << "Optimisation opportunity:" << std::endl;
+              peepholeSource->dump();
+              instruction->dump();
+              std::cout << std::endl;
+              
+              // Change the IFN to IFB and swap the operands
+              instruction->getOperand(0).setImm(DCPU16CC::COND_C);
+              swapOptBrcc(instruction, peepholeSource);
+              
+              // Remove the AND from the block
+              peepholeSource->eraseFromParent();
+              
+              std::cout << "Changed to:" << std::endl;
+              instruction->dump();
+              std::cout << "--------------------------------------------------------" << std::endl << std::endl;;
+            }
+            
+            break;
+          }
+        }
+      }
+    }
+  }
 }
 
 bool DCPU16Peephole::runOnMachineFunction(MachineFunction &MF) {
-	QII = static_cast<const DCPU16InstrInfo *>(MF.getTarget().getInstrInfo());
-	QRI = static_cast<const DCPU16RegisterInfo *>(MF.getTarget().getRegisterInfo());
-	MRI = &MF.getRegInfo();
-	
-	// Disable all peephole optimisations
-	if(DisableDCPU16Peephole) return false;
+  QII = static_cast<const DCPU16InstrInfo *>(MF.getTarget().getInstrInfo());
+  QRI = static_cast<const DCPU16RegisterInfo *>(MF.getTarget().getRegisterInfo());
+  MRI = &MF.getRegInfo();
+  
+  // Disable all peephole optimisations
+  if(DisableDCPU16Peephole) return false;
 
-	// Loop over all of the basic blocks.
-	for(MachineFunction::iterator mbbIter = MF.begin(); mbbIter != MF.end(); ++mbbIter) {
-		MachineBasicBlock *mbb = mbbIter;
-		
-		if(!DisableOptBrcc) runOptBrcc(mbb);
-		
-		/*// Search for AND, IFE
-		if (!DisableOptIFA) {
-			
-			// Traverse the basic block.
-			for (MachineBasicBlock::iterator MII = MBB->begin(); MII != MBB->end();
-																			 ++MII) {
-				MachineInstr *MI = MII;
-				
-				if (!DisableOptIFA && (
-					 MI->getOpcode() == DCPU16::AND16rm ||
-					 MI->getOpcode() == DCPU16::AND16ri ||
-					 MI->getOpcode() == DCPU16::AND16mr ||
-					 MI->getOpcode() == DCPU16::AND16mi ||
-					 MI->getOpcode() == DCPU16::AND16mm ||
-					 MI->getOpcode() == DCPU16::AND16rr
-				)) {
-					switch(MI->getOpcode()) {
-						case DCPU16::AND16ri: {
-							assert (MI->getNumOperands() == 4);
-							
-							MachineOperand &Dst = MI->getOperand(0);
-							MachineOperand &Src	= MI->getOperand(1);
-							unsigned DstReg = Dst.getReg();
-							unsigned SrcReg = Src.getReg();
-							
-							if (TargetRegisterInfo::isVirtualRegister(DstReg) &&
-								TargetRegisterInfo::isVirtualRegister(SrcReg)) {
-								PeepholeMap[DstReg] = SrcReg;
-							}
-						}
-					}
-				}
-				
-				if (!DisableOptIFA && (
-					 MI->getOpcode() == DCPU16::BR_CCri
-				)) {
-					switch (MI->getOperand(0).getImm()) {
-						case DCPU16CC::COND_NE: {
-							assert (MI->getNumOperands() == 4);
-							
-							if (MI->getOperand(2).getImm() != 0) break;
-							
-							MachineOperand &Src = MI->getOperand(1);
-							unsigned SrcReg = Src.getReg();
-							
-							if (TargetRegisterInfo::isVirtualRegister(SrcReg)) {
-								// Try to find in the map.
-								if (unsigned PeepholeSrc = PeepholeMap.lookup(SrcReg)) {
-									// Change the 1st operand.
-									MI->RemoveOperand(3);
-									MI->addOperand(MachineOperand::CreateImm(53264));
-									
-									std::cout << std::endl;
-									std::cout << "Optimise here plz" << std::endl;
-									MI->dump();
-									std::cout << std::endl;
-								}
-							}
-							
-							if (TargetRegisterInfo::isVirtualRegister(DstReg) &&
-								TargetRegisterInfo::isVirtualRegister(SrcReg)) {
-								PeepholeMap[DstReg] = SrcReg;
-							}
-						}
-					}
-				}
-			} // Instruction
-		}*/
-	} // Basic Block
-	return true;
+  // Loop over all of the basic blocks.
+  for(MachineFunction::iterator mbbIter = MF.begin(); mbbIter != MF.end(); ++mbbIter) {
+    MachineBasicBlock *mbb = mbbIter;
+    
+    if(!DisableOptBrcc) runOptBrcc(mbb);
+  } // Basic Block
+  return true;
 }
 
 FunctionPass *llvm::createDCPU16Peephole() {
-	return new DCPU16Peephole();
+  return new DCPU16Peephole();
 }
 
